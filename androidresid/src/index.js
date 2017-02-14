1d6d3d2f670f8cfd0b771202c0af2e242415947c
Rustem <Rustem.Saitkulov@agoda.com>
2017-02-14 16:12:50 +0700

    Add Firebase to your web app

--- androidresid/src/index.js
+++ androidresid/src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyANTaJBeWtEfrt7dcCPjx3N3uIx6AMLw-8",
+    authDomain: "neural-cathode-650.firebaseapp.com",
+    databaseURL: "https://neural-cathode-650.firebaseio.com",
+    storageBucket: "neural-cathode-650.appspot.com",
+    messagingSenderId: "309475002057"
+  };
+  firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
