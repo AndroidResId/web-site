901985c1e8732cb821155596ec030ee8618d6c0d
Rustem <Rustem.Saitkulov@agoda.com>
2017-02-15 11:10:23 +0700

    Update logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo-android-white} className="App-logo" alt="logo" />
           <h2>Welcome to AndroidResId</h2>
         </div>
         <p className="App-intro">
